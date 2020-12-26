ec61cfb20084c52c9b1f7bbbdf1b4ad9b0d9cc18
Aang23 <qwerty15@gmx.fr>
2020-12-26 16:21:47 +0100

    Add FFF FIR filter

--- src/fir_filter.cpp
+++ src/fir_filter.cpp
@@ -7,8 +7,6 @@ namespace libdsp
         d_fir = new fir_filter_ccf(decimation, taps);
         d_updated = false;
         decimation_m = decimation;
-
-        const int alignment_multiple = 1 / sizeof(float);
     }
 
     FIRFilterCCF::~FIRFilterCCF()
@@ -42,4 +40,43 @@ namespace libdsp
 
         return process_size / decimation_m;
     }
-}
+
+    FIRFilterFFF::FIRFilterFFF(int decimation, const std::vector<float> &taps)
+    {
+        d_fir = new fir_filter_fff(decimation, taps);
+        d_updated = false;
+        decimation_m = decimation;
+    }
+
+    FIRFilterFFF::~FIRFilterFFF()
+    {
+        delete d_fir;
+    }
+
+    void FIRFilterFFF::set_taps(const std::vector<float> &taps)
+    {
+        d_fir->set_taps(taps);
+        d_updated = true;
+    }
+
+    std::vector<float> FIRFilterFFF::taps() const
+    {
+        return d_fir->taps();
+    }
+
+    size_t FIRFilterFFF::work(float *in, size_t length, float *out)
+    {
+        tmp_.insert(tmp_.end(), in, &in[length]);
+
+        size_t process_size = tmp_.size() - d_fir->ntaps();
+
+        if (decimation_m == 1)
+            d_fir->filterN(out, tmp_.data(), process_size);
+        else
+            d_fir->filterNdec(out, tmp_.data(), process_size, decimation_m);
+
+        tmp_.erase(tmp_.begin(), tmp_.end() - d_fir->ntaps());
+
+        return process_size / decimation_m;
+    }
+} // namespace libdsp
--- src/fir_filter.h
+++ src/fir_filter.h
@@ -22,4 +22,23 @@ namespace libdsp
 
         size_t work(std::complex<float> *in, size_t length, std::complex<float> *out);
     };
-}
+
+    class FIRFilterFFF
+    {
+    private:
+        fir_filter_fff *d_fir;
+        bool d_updated;
+        int decimation_m;
+        std::vector<float> tmp_;
+
+    public:
+        FIRFilterFFF(int decimation, const std::vector<float> &taps);
+
+        ~FIRFilterFFF();
+
+        void set_taps(const std::vector<float> &taps);
+        std::vector<float> taps() const;
+
+        size_t work(float *in, size_t length, float *out);
+    };
+} // namespace libdsp
